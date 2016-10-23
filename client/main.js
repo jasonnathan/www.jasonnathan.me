104473c7d9517bf9d2c5fc09eeb271a945524c66
Jason Nathan <jjnathanjr@gmail.com>
2016-10-24 00:59:07 +0800

    Temporarily removed apollo related functionality

--- client/main.js
+++ client/main.js
@@ -1,17 +1,11 @@
+/* global document*/
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import React from 'react';
+import App from '/imports/ui/App.jsx';
+import 'react-flex/index.css';
 
-import ApolloClient from 'apollo-client';
-import { meteorClientConfig } from 'meteor/apollo';
-import { ApolloProvider } from 'react-apollo';
-
-import App from '/imports/ui/App';
-
-const client = new ApolloClient(meteorClientConfig());
 
 Meteor.startup(() => {
-  render(<ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>, document.getElementById('app'));
-});
+  render(<App />, document.getElementById('app'))}
+);
