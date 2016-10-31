cc2b1710db1da53d911dbbd2b790e98a7ddcdb29
Jason Nathan <jjnathanjr@gmail.com>
2016-10-31 19:14:54 +0800

    Added a default index.jsx for all components

---
+++ imports/ui/index.jsx
@@ -0,0 +1,8 @@
+import App from './App.jsx';
+import Home from './Home.jsx';
+import About from './About.jsx';
+import Work from './Work.jsx';
+import Contact from './Contact.jsx';
+import Blog from './Blog.jsx';
+
+export {App, Home, About, Work, Contact, Blog}
