ff030929621bcedbab8cba07720e9973ff3c4059
Jason Nathan <jjnathanjr@gmail.com>
2016-10-31 19:09:57 +0800

    Added a Category component

---
+++ imports/ui/components/Categories.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { graphql } from 'react-apollo';
+import {spring, presets, StaggeredMotion} from 'react-motion';
+import getCategories from '/imports/api/categories-query-gql';
+import Loader from 'react-loaders';
+
+const abstractCategoriesList = ({data}) =>{
+  if(data.loading)
+    return (<div className="centered-content" style={{paddingTop:'25vh'}}><Loader type="ball-triangle-path" /></div>);
+
+
+  return (
+    <StaggeredMotion
+      defaultStyles={[...data.categories].map(() => {return {h: 0.1}})}
+      styles={
+        prevStyles =>
+          prevStyles.map((_, i) => {
+            return { h: spring(i === 0 ? 1 : prevStyles[i - 1].h, {...presets.stiff, precision:.1})}})}
+    >
+      {interpolatingStyles => <div className="box"><h4 className="header">Categories</h4>
+        {interpolatingStyles.map((style, i) => {
+          let cat = data.categories[i],
+              catHTML = {__html: `${cat.name} (${cat.count})`}
+          return (
+            <a
+              key={cat.slug}
+              href={`/${cat.slug}`}
+              dangerouslySetInnerHTML={catHTML}
+            />
+          )
+        })}
+      </div>
+      }
+    </StaggeredMotion>
+  )
+}
+
+
+const CategoriesList = graphql(getCategories)(abstractCategoriesList);
+
+export default CategoriesList;
