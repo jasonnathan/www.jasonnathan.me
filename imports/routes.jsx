ed609ab3b03a3f22acb118ce72abc24360238a02
Jason Nathan <jjnathanjr@gmail.com>
2016-11-03 21:22:01 +0800

    tried to get SSR working for single blog posts. Disabled for now

--- imports/routes.jsx
+++ imports/routes.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import {App, Home, About, Work, Contact, Blog} from '/imports/ui/index.jsx';
+import {Meteor} from 'meteor/meteor';
+import {
+  App, Home, About, Work, Contact, Blog,
+  Article, PostsByCategory, PostWithComments
+} from '/imports/ui/index.jsx';
 import ApolloClient, {createNetworkInterface} from 'apollo-client';
 import {ApolloProvider} from 'react-apollo';
 import {getDataFromTree} from "react-apollo/server";
 import {InjectData} from 'meteor/meteorhacks:inject-data';
+import {Promise} from 'meteor/promise';
 
-let opts = { ssrMode: Meteor.isServer}, client
+let opts = { ssrMode: Meteor.isServer}, client;
 
 if (Meteor.isServer) {
   opts.networkInterface = createNetworkInterface('http://localhost:3000/graphql', {credentials: 'same-origin'});
@@ -14,16 +19,27 @@ if (Meteor.isServer) {
 } else {
 
   InjectData.getData("dehydrated-initial-data", (state) => {
-    client = new ApolloClient({...opts, initialState: JSON.parse(state)});
+      client = new ApolloClient({...opts, initialState: JSON.parse(state)});
   });
 }
 
 
 const BlogWithApollo = () => ( <ApolloProvider client={client}><Blog /></ApolloProvider>);
+const ArticleWithApollo = (p) => (<ApolloProvider client={client}><Article {...p} /></ApolloProvider>);
 
 const preRender = (req) => {
-  if (req.originalUrl === '/articles')
-    return Promise.await(getDataFromTree(<BlogWithApollo />));
+    const segments = req.originalUrl.split('/').filter(p => p),
+          controller = segments[0],
+          category = segments[1],
+          slug = segments[2],
+          props = {
+            routeParams: {category, slug}
+          };
+
+    switch(controller){
+      case 'articles': return Promise.await(getDataFromTree(<BlogWithApollo />));
+      case 'article': return Promise.await(getDataFromTree(<ArticleWithApollo {...props} />));
+    }
 }
 
 const AppRoutes = (
@@ -32,8 +48,13 @@ const AppRoutes = (
     <Route path="about" component={About} />
     <Route path="work" component={Work} />
     <Route path="contact" component={Contact} />
-    <Route path="articles" component={BlogWithApollo} />
-    {/* ... */}
+    <Route path="articles">
+      <IndexRoute component={BlogWithApollo} />
+      <Route path=":category" component={PostsByCategory} />
+    </Route>
+    <Route path="article">
+      <Route path=":category/:slug" component={ArticleWithApollo} />
+    </Route>
   </Route>
 );
 
