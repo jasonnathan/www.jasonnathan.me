6e2666c71a8767b347eaeffb8df5368ab97ce479
Jason Nathan <jjnathanjr@gmail.com>
2016-11-03 21:35:23 +0800

    Added more components to the default export

--- imports/ui/index.jsx
+++ imports/ui/index.jsx
@@ -4,5 +4,8 @@ import About from './About.jsx';
 import Work from './Work.jsx';
 import Contact from './Contact.jsx';
 import Blog from './Blog.jsx';
+import PostsByCategory from './PostsByCategory.jsx';
+import Article from './Post/Article.jsx';
+import PostWithComments from './Post/PostWithComments.jsx';
 
-export {App, Home, About, Work, Contact, Blog}
+export {App, Home, About, Work, Contact, Blog, Article, PostsByCategory, PostWithComments}
