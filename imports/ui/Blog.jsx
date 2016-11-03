902e7fb718336d8cef29bf0d49aecb0c2c07b08f
Jason Nathan <jjnathanjr@gmail.com>
2016-11-03 21:22:48 +0800

    added a classname to the blog and other transition changes

--- imports/ui/Blog.jsx
+++ imports/ui/Blog.jsx
@@ -7,12 +7,13 @@ import StaggeredName from './components/StaggeredName.jsx';
 import MainMenu from './components/MainMenu.jsx';
 import PostsList from './components/Posts.jsx';
 import CategoriesList from './components/Categories.jsx';
+import { fade} from 'react-router-transitioner';
 
 // Create the client as outlined above
 const popConfig = { stiffness: 360, damping: 25 };
 
 const Blog = () => {
-  return (<Flex row alignItems="flex-start" justifyContent="center" style={{height: '100%'}}>
+  return (<Flex row alignItems="flex-start" className="noscroll" justifyContent="center" style={{height: '100%'}}>
         <Helmet
           title="Articles | Jason J. Nathan"
           meta={[
@@ -20,7 +21,7 @@ const Blog = () => {
           ]}
         />
           <div id="start-screen-container" style={{paddingTop:'80px', height:'100vh'}}>
-            <StaggeredName letters="Articles" />
+            <StaggeredName letters="All Articles" />
             <Flex alignItems="flex-start">
               <Item flex={2}><PostsList /></Item>
               <Item column wrap alignContent="space-between" justifyContent="space-between" className="hidden-mobile" flex={1}>
@@ -35,23 +36,7 @@ const Blog = () => {
       </Flex>)
 }
 
-Blog.sceneConfig = {
-  atEnter: {
-    opacity: 1
-  },
-  atLeave: {
-    opacity: spring(0, popConfig)
-  },
-  atActive: {
-    opacity: 1
-  },
-  mapStyles(styles) {
-    return {
-      height:'100%',
-      opacity: styles.opacity
-    };
-  }
-}
+Blog.sceneConfig = fade
 
 
 
