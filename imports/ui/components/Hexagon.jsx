c62f8e1b256bb2856a34073d702bbd3b07195884
Jason Nathan <jjnathanjr@gmail.com>
2016-11-09 00:25:01 +0800

    Experimenting with hexagons

---
+++ imports/ui/components/Hexagon.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class Hexagon extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div className="hexagon" />
+    )
+  }
+}
