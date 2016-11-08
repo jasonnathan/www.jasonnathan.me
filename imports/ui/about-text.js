636ea0ef5d169038eaafb47c13e5bd64027f0040
Jason Nathan <jjnathanjr@gmail.com>
2016-11-09 00:21:55 +0800

    Moved data for the About page to its own file

---
+++ imports/ui/about-text.js
@@ -0,0 +1,44 @@
+export const person = [
+  "A burgeoning entrepreneur and service-oriented professional whose passion lies in innovation, engineering and user experiences.",
+  "Possesses over ten years of experience in Software Engineering and has an outgoing personality.",
+  "Commands an immense flair for communication skills to connect with a wide range of audiences.",
+  "Driven to be self-trained, displays enthusiasm when exploring new ideas and excels beyond paper qualifications.",
+  "A captivating and charismatic presenter of ideas and a valuable asset to his team.",
+  "A natural leader who can inspire, nurture and produce results."
+];
+export const website = [
+  "This site is written in ES6 with Meteor, GraphQL and ReactJS. The code lives in a public repository in Github",
+  "It is deployed on an Ubuntu server in Digital Ocean, loads static assets from Amazon Cloudfront and is served via Nginx proxying in front of a NodeJS server. The SSL certificate is free, provided for by LetsEncrypt.",
+  "It has been designed & built from scratch upon a base of selected NodeJS & Meteor modules:",
+  "react-router, react-motion, react-apollo and apollostack to name a few.",
+  "The technology powering this site can very easily power most Android/iOS mobile applications.",
+  "For resource-heavy applications like games, there is React Native."
+];
+export const facts = [
+  {
+    label: "1st Computer",
+    src: "IBM.svg",
+    link: "https://en.wikipedia.org/wiki/Intel_80486DX2",
+    text: "486 DX2 66 MHz, 8MB RAM"
+  }, {
+    label: "1st DataBase",
+    src: "DBase.svg",
+    link: "http://www.eweek.com/database/30-years-ago-the-rise-fall-and-survival-of-ashton-tates-dbase.html",
+    text: "III"
+  }, {
+    label: "1st Script",
+    src: "BASIC.svg",
+    link: "http://time.com/69316/basic/",
+    text: ", QBAsic"
+  }, {
+    label: "Favourite Editor",
+    src: "atom.svg",
+    link: "https://atom.io",
+    text: "Editor"
+  }, {
+    label: "Favourite OS",
+    src: "ubuntu.svg",
+    link: "https://www.ubuntu.com",
+    text: ""
+  }
+];
