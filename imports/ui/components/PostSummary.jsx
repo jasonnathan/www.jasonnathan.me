b964453243ff8c0a22a874029593eee3da5bdd2b
Jason Nathan <jjnathanjr@gmail.com>
2016-11-16 07:45:50 +0800

    Added prefetch for individual posts to load the page faster

--- imports/ui/components/PostSummary.jsx
+++ imports/ui/components/PostSummary.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import {withApollo} from 'react-apollo';
 import truncatise from 'truncatise';
+import getPostBySlug from '/imports/api/post-by-slug-gql';
+
 import FadeInImage from './FadeInImage.jsx';
 
-const PostSummary = ({post}) => {
+
+const PostSummary = ({post, client}) => {
   const getUrl = () => {
     const cat = post.categories[0].slug;
     return `/article/${cat}/${post.slug}`;
   }
+  const prefetchPost = () => {
+    client.query({
+      query: getPostBySlug,
+      variables: {slug: post.slug}
+    })
+
+  }
   return(
-    <a href={getUrl()}>
+    <a href={getUrl()} onMouseOver={prefetchPost}>
       <article>
         <FadeInImage size="100%" className="bg-image" src={post.featured_media_url} />
         <div className="post-content">
@@ -20,4 +31,4 @@ const PostSummary = ({post}) => {
   )
 }
 
-export default PostSummary;
+export default withApollo(PostSummary);
