86045bdc56a4013538ddd6c907f7a98f23ee54c8
Jason Nathan <jjnathanjr@gmail.com>
2016-11-15 23:46:19 +0800

    Fixed component and cleaned up a little

--- imports/ui/Post/PostWithComments.jsx
+++ imports/ui/Post/PostWithComments.jsx
@@ -1,77 +1,98 @@
 import React from 'react';
+import {Meteor} from 'meteor/meteor';
 import {graphql} from 'react-apollo';
 import {Flex, Item} from 'react-flex';
 import Loader from 'react-loaders';
+import {Link} from 'react-router';
+import entities from 'entities';
 import getPostBySlug from '/imports/api/post-by-slug-gql';
 import ReactDisqusThread from './ReactDisqusThread.jsx';
-import MainMenu from '../components/MainMenu.jsx';
 import CategoriesList from '../components/Categories.jsx';
+import BreadCrumbsHeader from '../components/BreadCrumbsHeader.jsx';
 
-let lc = 1;
+
+const postResolver = (key, text) => {
+  return props => {
+    switch(key){
+      case ':category':
+        return props.category;
+      case ':slug':
+        return props.title;
+    }
+    return text;
+  };
+};
+
+const lastCrumbIsString = (link, key, text, index, routes) => {
+  if (index === routes.length -1) {
+    return <span key={key}>{text}</span>;
+  }
+  return <Link to={link} key={key}>{text}</Link>;
+}
 
 const abstractPostWithComments = (props) => {
   const {loading, post, error} = props.data;
   const {location} = props;
+  const {routes, params} = props;
+  const getHtml = (val) => ({__html: val});
 
   if(loading)
-      return (<div className="centered-loader" style={{paddingTop:'25vh'}}>
-        <Loader type="ball-triangle-path" />
-      </div>);
+    return (<div className="centered-loader" style={{paddingTop:'25vh'}}>
+      <Loader type="ball-triangle-path" />
+    </div>);
+  if(error)
+    throw error
 
-  if (error) {
-    const result = {
-      __html: JSON.stringify(error, null, 2).replace("\n", "<br />")
-    }
-    return (<pre style={{maxWidth:'75vh'}} dangerouslySetInnerHTML={result} />)
+  const crumbs = {
+    title: entities.decodeHTML(post.title.rendered),
+    category: entities.decodeHTML(post.categories[0].name)
   }
-  lc++;
-  if(!post)
-    return (<pre style={{maxWidth:'75vh'}} dangerouslySetInnerHTML={{__html:JSON.stringify(props, null, 2)}} />)
-  const article = post[0];
-  const getHtml = (val) => ({__html: val});
+
   return (
-    <div className="scroll-y">
-      <Flex alignItems="flex-start">
-        <Item className="single-post" flex={2}>
-          <article className="post-content">
-            <header><h3 dangerouslySetInnerHTML={getHtml(article.title.rendered)} /></header>
-            <section dangerouslySetInnerHTML={getHtml(article.content.rendered)} />
-            {Meteor.isClient && (
-              <ReactDisqusThread
-                identifier={article.slug}
-                title={article.title.rendered}
-                url={`http://dev.jasonnathan.com/${location.pathname}`}
-              />)}
-          </article>
-        </Item>
-        <Item
-          column
-          wrap
-          alignContent="space-between"
-          justifyContent="space-between"
-          className="hidden-mobile"
-          flex={1}
-        >
-          <CategoriesList />
-        </Item>
-      </Flex>
+    <div role="main">
+      <BreadCrumbsHeader routes={routes} params={params} resolver={postResolver} crumbs={crumbs} lastCrumbResolver={lastCrumbIsString} />
+      <div className="content with-breadcrumbs">
+        <div className="scroll-y">
+          <Flex alignItems="flex-start" className="responsive">
+            <Item className="single-post" flex={2}>
+              <article className="post-content">
+                <header><h3 dangerouslySetInnerHTML={getHtml(post.title.rendered)} /></header>
+                <section dangerouslySetInnerHTML={getHtml(post.content.rendered)} />
+                <ReactDisqusThread
+                  identifier={post.slug}
+                  title={post.title.rendered}
+                  url={`http://dev.jasonnathan.com/${location.pathname}`}
+                />
+              </article>
+            </Item>
+            <Item
+              column
+              wrap
+              alignContent="space-between"
+              justifyContent="space-between"
+              className="hidden-mobile"
+              flex={1}
+            >
+              <CategoriesList />
+            </Item>
+          </Flex>
+        </div>
+      </div>
     </div>
   )
 }
 
-// We then can use `graphql` to pass the query results returned by MyQuery
-// to MyComponent as a prop (and update them as the results change)
 const PostWithComments = graphql(getPostBySlug, {
   options: (ownProps) => {
-    if(!ownProps.routeParams)
+    if(!ownProps.params)
       return {};
 
-    return {variables: {
-        slug:ownProps.routeParams.slug
-      }}
-  },
-  props:(props) => {
-    return props;
+    return {
+      variables: {
+        slug: ownProps.params.slug
+      },
+      ssr: Meteor.isServer
+    }
   }
 })(abstractPostWithComments);
 export default PostWithComments;
