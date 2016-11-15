4309138ebf8e6b93359d581ea3c118aa0fa1bb4d
Jason Nathan <jjnathanjr@gmail.com>
2016-11-15 23:47:25 +0800

    Cleaned up isomorphic index page

--- index.jsx
+++ index.jsx
@@ -1,82 +1,37 @@
-/*globals ga*/
+/*globals ga, document*/
 import React from 'react';
 import {Meteor} from 'meteor/meteor';
-import {ReactRouterSSR} from 'meteor/reactrouter:react-router-ssr';
+import {ReactRouterSSR} from 'meteor/jasonnathan:react-router-ssr';
 import ReactHelmet from 'react-helmet';
-// import {AppRoutes, preRender, client} from '/imports/routes.jsx';
-// import React from 'react';
-import { Route, IndexRoute } from 'react-router';
-// import {Meteor} from 'meteor/meteor';
-import {
-  App, Home, About, Work, Contact, Blog,
-  Article, PostsByCategory, PostWithComments
-} from '/imports/ui/index.jsx';
+import AppRoutes from '/imports/routes.jsx';
 import ApolloClient, {createNetworkInterface} from 'apollo-client';
 import {ApolloProvider} from 'react-apollo';
 import {getDataFromTree} from "react-apollo/server";
-import {InjectData} from 'meteor/meteorhacks:inject-data';
-import {Promise} from 'meteor/promise';
 
-let history;
-let opts = { ssrMode: Meteor.isServer}, client;
+let opts = {
+  ssrMode: Meteor.isServer,
+  networkInterface: createNetworkInterface({credentials: 'same-origin', uri: 'http://localhost:3000/graphql'})
+}, client, initialState;
 
-if (Meteor.isServer) {
-  opts.networkInterface = createNetworkInterface('http://localhost:3000/graphql', {credentials: 'same-origin'});
+
+const rehydrateHook = state => initialState = state;
+
+// the dumbest thing, to put wrapperHook in clientOptions, infering it only runs on the client
+const wrapperHook = app => {
+  opts.initialState = initialState;
   client = new ApolloClient(opts);
-} else {
-
-  InjectData.getData("dehydrated-initial-data", (state) => {
-      client = new ApolloClient({...opts, initialState: JSON.parse(state)});
-  });
-}
-
-
-const BlogWithApollo = (props) => ( <ApolloProvider client={client}><Blog {...props} /></ApolloProvider>);
-const ArticleWithApollo = (props) => (<ApolloProvider client={client}><Article {...props} /></ApolloProvider>);
-
-const preRender = (req) => {
-    const segments = req.originalUrl.split('/').filter(p => p),
-          controller = segments[0],
-          category = segments[1],
-          slug = segments[2],
-          props = {slug, category};
-
-    switch(controller){
-      case 'articles': return Promise.await(getDataFromTree(<BlogWithApollo />));
-      case 'article': return Promise.await(getDataFromTree(<ArticleWithApollo />));
-    }
-}
-
-const AppRoutes = (
-  <Route path="/" component={App}>
-    <IndexRoute component={Home} />
-    <Route path="about" component={About} />
-    <Route path="work" component={Work} />
-    <Route path="contact" component={Contact} />
-    <Route path="articles">
-      <IndexRoute component={BlogWithApollo} />
-      <Route path=":category" component={PostsByCategory} />
-    </Route>
-    <Route path="article">
-      <Route path=":category/:slug" component={ArticleWithApollo} />
-    </Route>
-  </Route>
-);
-
-
-// export  {AppRoutes, preRender, client};
-const dehydrateHook = () => client.store ? client.store.getState() : null;
-const historyHook = newHistory => history = newHistory;
-
-
+  return <ApolloProvider client={client}>{app}</ApolloProvider>
+};
+const preRender = (req, res, app) => Promise.await(getDataFromTree(app));
+const dehydrateHook = () => ({apollo:{data:client.store.getState().apollo.data}})
 const htmlHook = html => {
   const head = ReactHelmet.rewind();
   return html.replace('<head>', '<head>' + head.title + head.base + head.meta + head.link + head.script);
 }
 
-const props = {onUpdate: () => Meteor.isClient && ga('send', 'pageview')}
+// const props = {onUpdate: () => Meteor.isClient && ga('send', 'pageview')}
 
-const clientOptions = {historyHook, props};
-const serverOptions = {historyHook, htmlHook, preRender, dehydrateHook};
+const clientOptions = {wrapperHook, rehydrateHook};
+const serverOptions = {htmlHook, preRender, dehydrateHook};
 
-ReactRouterSSR.Run(AppRoutes, clientOptions, serverOptions);
+ReactRouterSSR.Run(AppRoutes(), clientOptions, serverOptions);
