b3d2196e6ad6d0bf2769d912e0cfd373af54bc65
Jason Nathan <jjnathanjr@gmail.com>
2016-11-17 21:53:44 +0800

    removed form-data dependancy and used query string instead

--- imports/api/graphql/wp-connector.js
+++ imports/api/graphql/wp-connector.js
@@ -1,6 +1,6 @@
 /*globals fetch*/
 import 'isomorphic-fetch';
-import FormData from 'form-data';
+import querystring from 'querystring';
 
 // Posts.remove({});
 
@@ -8,46 +8,60 @@ import FormData from 'form-data';
 const url = 'https://public-api.wordpress.com/wp/v2/sites/jasonnathan.wordpress.com';
 const authUrl = 'https://public-api.wordpress.com/oauth2/token';
 let _headers = {
-  Accept: 'application/json',
-  'Content-Type': 'application/json',
+  // Accept: 'application/json',
+  // 'Content-Type': 'application/json',
   Authorization: null
 };
 
 const authWP = () => {
-  if(_headers.Authorization)
+  if (_headers.Authorization)
     return Promise.resolve(_headers);
 
-  const form = new FormData();
-  const { CLIENT_ID, CLIENT_SECRET,WP_USERNAME,WP_PASSWD } = process.env;
-  form.append('client_id', CLIENT_ID);
-  form.append('client_secret', CLIENT_SECRET);
-  form.append('grant_type', 'password');
-  form.append('username', WP_USERNAME);
-  form.append('password', WP_PASSWD);
-
-  return fetch(authUrl, {method: 'POST', compress: true, body: form})
-          .then(res => res.json())
-          .then(({access_token}) => {_headers.Authorization = `Bearer ${access_token}`; return _headers})
+  const {CLIENT_ID, CLIENT_SECRET, WP_USERNAME, WP_PASSWD} = process.env;
+  const form = {
+    client_secret: CLIENT_SECRET, client_id: +CLIENT_ID,
+    grant_type: 'password', username: WP_USERNAME, password: WP_PASSWD
+  };
+  return fetch(authUrl, {
+    headers: {"Content-Type": 'application/x-www-form-urlencoded'},
+    method: 'POST',
+    compress: true,
+    body: querystring.stringify(form)
+  })
+  .then(res => res.json())
+  .then(({access_token}) => ({Authorization:`Bearer ${access_token}`}))
 }
-const getWP = (endpoint, query) => {
-  const ep = `${url}/${endpoint}` + (query ? `?${query}` : '');
-  return authWP().then((headers) => fetch(ep, {headers, compress: !0}).then( res => {
+const getWP = (endpoint, query='') => {
+  const ep = `${url}/${endpoint}`;
+  return authWP()
+  .then(headers => fetch(ep, {
+    body:query,
+    headers,
+    compress: !0
+  })
+  .then(res => {
     const totalPosts = res.headers.get('x-wp-total');
-    if(totalPosts)
-      return res.json().then((data) =>{
+    if (totalPosts)
+      return res.json().then((data) => {
         data.totalPosts = totalPosts;
         return Promise.resolve(data);
       });
     return res.json();
-  } ))
+  }))
+  // .then(data => {
+  //   console.log(data)
+  //   return Promise.resolve(data);
+  // })
 }
 
 export const getPost = args => {
-  const then = (posts) => Promise.resolve(posts.length ? posts[0] : {});
-  if(args.id){
+  const then = (posts) => Promise.resolve(posts.length
+    ? posts[0]
+    : {});
+  if (args.id) {
     return getWP(`posts/${args.id}`).then(then);
   }
-  if(args.slug){
+  if (args.slug) {
     return getWP('posts', `slug=${args.slug}&context=edit`).then(then);
   }
 }
