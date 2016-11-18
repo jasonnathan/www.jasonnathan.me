ce9b7f81d0aef16d82b4c40ec4c4c85e903561aa
Jason Nathan <jjnathanjr@gmail.com>
2016-11-18 11:55:54 +0800

    WIP working on skills page

--- imports/ui/Work/Skill.jsx
+++ imports/ui/Work/Skill.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router';
-// import {Motion, presets, spring} from 'react-motion';
+import {StaggeredMotion, spring} from 'react-motion';
 import {SkillsData, skurl} from '/imports/api/data/skills-data';
 import BreadCrumbsHeader from '../components/BreadCrumbsHeader.jsx';
 import FooterTransition from '../components/FooterTransition.jsx';
@@ -23,44 +23,63 @@ export default class Skill extends Component{
     this.state = {
       open: true
     }
+    this.lines = skillItem(props.params.skill).overview.split("<br />");
   }
 
-  openSideBar(active, e){
-    e.stopPropagation();
-    this.setState({
-      open: active
-    });
+  staggeredProps(){
+    const p = this.lines;
+    const _s = { stiffness: 900, damping: 50, precision:.1 }
+    return {
+      defaultStyles: p.map(() => ({t:50, o:0.1})),
+      styles: prevInterpolatedStyles => prevInterpolatedStyles.map((_,i) => {
+        return i === 0
+              ? {t: spring(0, _s), o:spring(1, _s)}
+              : {t: spring(prevInterpolatedStyles[i - 1].t, _s), o:spring(prevInterpolatedStyles[i - 1].o)}
+      })
+    }
+  }
+
+  staggeredStyle({t,o}){
+    return {transform:`translate3d(0,${t}%,0)`, margin:"1rem auto 0 auto", opacity:o}
   }
 
   render(){
     const {routes, params} = this.props;
     const skill = skillItem(params.skill);
-    const projs = [1, 2,3,4,5,6,7,8,9,10,11];
+    const projs = skill.projects;
     return (
-      <div role="main" style={{position:"fixed", top:0, left:0, width:"100vw", height:"100%", bottom:0,background:"#222"}}>
-        <BreadCrumbsHeader routes={routes} params={params} resolver={skillResolver} crumbs={skill.title} lastCrumbResolver={lastCrumbIsString} />
-        {/* <aside className={`leftbar ${isOpen}`}>
-          <header className="header" onClick={(e) => this.openSideBar(true, e)}>
-            <h4>SideBar</h4>
-            <button className="close" onClick={(e) => this.openSideBar(false, e)}>X</button>
-          </header>
-          <section className="content with-header">
-            <p>This {params.skill.title} is the coolest thing in the world</p>
-          </section>
-        </aside> */}
+      <div role="main" style={{position:"fixed", top:0, left:0, width:"100vw", height:"100%", bottom:0, background:"#222"}}>
+        <BreadCrumbsHeader
+          routes={routes}
+          params={params}
+          goBack={this.props.router.goBack}
+          resolver={skillResolver}
+          crumbs={skill.title}
+          lastCrumbResolver={lastCrumbIsString}
+        />
         <div className="content with-breadcrumbs with-footer">
-          <div className="scroll-y" />
+          <div className="scroll-y">
+            <StaggeredMotion {...this.staggeredProps()}>
+              {interpolatedStyles =>
+                <article className="single-post skill-description">
+                {interpolatedStyles.map((style, i) => (
+                  <p
+                    key={i}
+                    style={this.staggeredStyle(style)}
+                    dangerouslySetInnerHTML={{__html: this.lines[i]}}
+                  />
+                ))}
+                </article>
+              }
+            </StaggeredMotion>
+          </div>
         </div>
         <FooterTransition>
-          <ul className="bottom-tabs" style={{minWidth:`${(projs.length * 154)}px`}}>
-            <li><Link to={skill.to} className="selected">Overview</Link></li>
-            {projs.map(p => {
-              return (
-                <li key={p}>
-                  <Link to={skill.to}>Private Property Singapore {p}</Link>
-                </li>
-              )
-            })}
+          <ul className="bottom-tabs" style={{maxWidth:`${(projs.length + 1) * 170}px`}}>
+            <li><Link to={skill.to} activeClassName="active">Overview</Link></li>
+            {projs.map(p => (
+              <li key={p.path}><Link activeClassName="selected" to={p.path}>{p.name}</Link></li>
+            ))}
           </ul>
         </FooterTransition>
       </div>
