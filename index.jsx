89b2a5f708e2f7b668e1eb3315a3be4f0b4bd7c6
Jason Nathan <jjnathanjr@gmail.com>
2016-11-23 06:18:43 +0800

    added some meteor friendly instantiation

--- index.jsx
+++ index.jsx
@@ -5,42 +5,52 @@ import {ReactRouterSSR} from 'meteor/jasonnathan:react-router-ssr';
 import ReactHelmet from 'react-helmet';
 import AppRoutes from '/imports/routes.jsx';
 import ApolloClient, {createNetworkInterface} from 'apollo-client';
+import { meteorClientConfig } from 'meteor/apollo';
 import {ApolloProvider} from 'react-apollo';
 import {getDataFromTree} from "react-apollo/server";
 
-let url = "http://localhost:3000";
-if(Meteor.isClient && process.env.NODE_ENV === 'production'){
-  url = "https://dev.jasonnathan.com";
-}
+// let url = "http://localhost:3000";
+// if(Meteor.isClient && process.env.NODE_ENV === 'production'){
+//   url = "https://dev.jasonnathan.com";
+// }
 
 let opts = {
   ssrMode: Meteor.isServer,
   networkInterface: createNetworkInterface({
-    credentials: 'same-origin', uri: `${url}/graphql`
+    credentials: 'same-origin', uri: `/graphql`
   })
-}, client, initialState, history;
+},
+client, initialState, history;
 
 
-const rehydrateHook = state => initialState = state;
-const historyHook = newHistory => history = newHistory;
-// the dumbest thing, to put wrapperHook in clientOptions, inferring it only
-// runs on the client
-const wrapperHook = app => {
-  opts.initialState = initialState;
-  client = new ApolloClient(opts);
-  return <ApolloProvider client={client}>{app}</ApolloProvider>
-};
-const preRender = (req, res, app) => Promise.await(getDataFromTree(app));
-const dehydrateHook = () => ({apollo:{data:client.store.getState().apollo.data}})
-const htmlHook = html => {
-  const h = ReactHelmet.rewind();
-  return html
-    .replace('<head>', '<head>' + h.title + h.base + h.meta + h.link + h.script);
-}
+Meteor.startup(() => {
+  const rehydrateHook = state => initialState = state;
+  const historyHook = newHistory => history = newHistory;
+  // the dumbest thing, to put wrapperHook in clientOptions, inferring it only
+  // runs on the client
+  const wrapperHook = app => {
+    opts.initialState = initialState;
+    if(Meteor.isClient){
+      client = new ApolloClient(meteorClientConfig());
+    } else {
+      client = new ApolloClient(opts);
+    }
+    return <ApolloProvider client={client}>{app}</ApolloProvider>
+  };
+  const preRender = (req, res, app) => Promise.await(getDataFromTree(app));
+  const dehydrateHook = () => ({apollo:{data:client.store.getState().apollo.data}})
+  const htmlHook = html => {
+    const h = ReactHelmet.rewind();
+    return html
+      .replace('<head>', '<head>' + h.title + h.base + h.meta + h.link + h.script);
+  }
 
-// const props = {onUpdate: () => Meteor.isClient && ga('send', 'pageview')}
+  // const props = {onUpdate: () => Meteor.isClient && ga('send', 'pageview')}
 
-const clientOptions = {historyHook, wrapperHook, rehydrateHook};
-const serverOptions = {historyHook, htmlHook, preRender, dehydrateHook};
+  const clientOptions = {historyHook, wrapperHook, rehydrateHook};
+  const serverOptions = {historyHook, htmlHook, preRender, dehydrateHook};
 
-ReactRouterSSR.Run(AppRoutes(), clientOptions, serverOptions);
+  ReactRouterSSR.Run(AppRoutes(), clientOptions, serverOptions);
+});
+
+export default client;
