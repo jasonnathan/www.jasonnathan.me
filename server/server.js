b6c839617618b99d54c0043851c2822ed1dee199
Jason Nathan <jjnathanjr@gmail.com>
2016-11-23 12:39:46 +0800

    added default types to exports

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 import { createApolloServer } from 'meteor/apollo';
 import { makeExecutableSchema } from 'graphql-tools';
-import { SchemaDefinition, RootQuery, Post, Author, resolvers } from '/imports/api/graphql/schema';
+import { SchemaDefinition, RootQuery, User, Post, Author, Category, Skill, resolvers } from '/imports/api/graphql/schema';
 import seed from './seed';
 
 const schema = makeExecutableSchema({
-  typeDefs: [SchemaDefinition, RootQuery, Post, Author],
+  typeDefs: [SchemaDefinition, RootQuery, User, Post, Author, Category, Skill],
   resolvers
 });
 
