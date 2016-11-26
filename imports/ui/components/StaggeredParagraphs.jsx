ed13dedf25844c4cefddb571288907c7f88f0bba
Jason Nathan <jjnathanjr@gmail.com>
2016-11-26 11:57:54 +0800

    Added comments & types StaggeredParagraphs

--- imports/ui/components/StaggeredParagraphs.jsx
+++ imports/ui/components/StaggeredParagraphs.jsx
@@ -1,14 +1,40 @@
-import React, {PureComponent} from 'react';
+/**
+ * @class StaggeredParagraphs
+ * @extends React.PureComponent
+ * @description Presentational Component for animated paragraphs used in Skills
+ */
+import React, {PureComponent, PropTypes} from 'react';
+import stylePropType from 'react-style-proptype';
 import {StaggeredMotion, spring} from 'react-motion';
 
 export default class StaggeredParagraphs extends PureComponent{
+  /**
+   * Default React constructor
+   */
   constructor(props){
     super(props);
   }
+
+  /**
+   * A simple method that splits a string into an array delimited by <br /> tags
+   * This is then used to animate the paragraphs on load
+   *
+   * @param {String} a HTML string
+   * @returns {Array} of split strings
+   */
   splitOnBreak(content){
     return content.split("<br />");
   }
 
+  /**
+   * This returns interpolation values for use in react-motion's StaggeredMotion
+   * Component. It requires a simple empty array with the number of items
+   * equivalent to the number of animatable elements. Given the content array
+   * generated above for convenience.
+   *
+   * @param {Array} an array of things to animate.
+   * @returns {Object} an object containing the required props for the Component
+   */
   staggeredProps(contentArr){
     const p = [...contentArr];
     const _s = { stiffness: 900, damping: 50, precision:.1 }
@@ -22,14 +48,30 @@ export default class StaggeredParagraphs extends PureComponent{
     }
   }
 
+  /**
+   * returns an array of props (normally styles) for use in the parent container
+   * of the animation. This method retrieves it from props and provides default
+   * if none was defined
+   *
+   * @returns {Object} of props
+   */
   containerProps(){
-    const {containerProps = {
-      className: "single-post skill-description",
-      style:{backgroundImage: "url(/green-bg.svg)"}
-    }} = this.props;
+    const {
+      containerProps = {
+        className: "single-post skill-description",
+        style:{backgroundImage: "url(/green-bg.svg)"}
+      }
+    } = this.props;
     return containerProps;
   }
 
+  /**
+   * This is the method that defines the styles for use within the animating
+   * elements. The properties are provided for by the interpolation
+   *
+   * @param {Oject: {t:Number, o:Number}} the current value of the interpolation
+   * @returns {Object:{style:{}}} the style object to use in the elements animating
+   */
   staggeredStyle({t,o}){
     return {transform:`translate3d(0,${t}%,0)`, margin:"1rem auto 0 auto", opacity:o}
   }
@@ -52,4 +94,12 @@ export default class StaggeredParagraphs extends PureComponent{
       </StaggeredMotion>
     )
   }
+}
+
+StaggeredParagraphs.propTypes = {
+  description: PropTypes.string.isRequired,
+  containerProps: PropTypes.shape({
+    className: PropTypes.string,
+    style: stylePropType
+  })
 }
