4ab39aa3b8dd5a23f8ebdaff2049998efd4c12c1
Jason Nathan <jjnathanjr@gmail.com>
2016-11-28 10:10:57 +0800

    removed rendered property

--- imports/ui/components/PostSummary.jsx
+++ imports/ui/components/PostSummary.jsx
@@ -24,8 +24,8 @@ const PostSummary = ({post, client}) => {
       <article style={{background:`url(${post.featured_media_url})`, backgroundSize:"cover", backgroundPosition:"50% 50%"}}>
         {/* <FadeInImage size="100%" className="bg-image" src={post.featured_media_url} /> */}
         <div className="post-content">
-          <h3 role="heading" id={`#${post.slug}`} dangerouslySetInnerHTML={{__html: post.title.rendered}} />
-          <p dangerouslySetInnerHTML={{__html: truncatise(post.excerpt.rendered, {TruncateLength:40})}} />
+          <h3 role="heading" id={`#${post.slug}`} dangerouslySetInnerHTML={{__html: post.title}} />
+          <p dangerouslySetInnerHTML={{__html: truncatise(post.excerpt, {TruncateLength:40})}} />
         </div>
       </article>
     </a>
