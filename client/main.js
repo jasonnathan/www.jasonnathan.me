195f2a0f16a14b18cbaefe705d099a067583d157
Jason Nathan <jjnathanjr@gmail.com>
2016-11-29 19:43:32 +0800

    Fixed removed fixtures and experiments & some cleanup

--- client/main.js
+++ client/main.js
@@ -1,11 +1,4 @@
 /*globals window, document, ga*/
-import {Meteor} from 'meteor/meteor';
-// import {resize, start} from '/imports/ui/canvasAnimation';
-
-Meteor.startup(() => {
-  // resize();
-  // setTimeout(() => start(), 3e3);
-});
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
